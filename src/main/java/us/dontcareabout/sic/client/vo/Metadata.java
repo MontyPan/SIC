d6a9aa420d974675c3de10dce074d75df57983f9
MontyPan <monty.pan@dontcareabout.us>
2026-04-23 21:34:23 +0800

    增加：getId()

--- src/main/java/us/dontcareabout/sic/client/vo/Metadata.java
+++ src/main/java/us/dontcareabout/sic/client/vo/Metadata.java
@@ -20,4 +20,12 @@ public final class Metadata extends Row {
 	public int getImgAmount() {
 		return intField("圖片數量");
 	}
+
+	/**
+	 * 以 {@link #getSerial()} 跟 {@link #getPage()} 的組合來作為 id。
+	 * 就商業邏輯來說是不會重複的，有的話是 sheet 維護的責任不是程式的責任 XD
+	 */
+	public String getId() {
+		return getSerial() + "_" + getPage();
+	}
 }
