53c97432bcfd1fa3b776d8d8e63d955a4b41a39d
MontyPan <monty.pan@dontcareabout.us>
2026-04-24 16:32:16 +0800

    增加：ImageHost

---
+++ src/main/java/us/dontcareabout/sic/client/util/ImageHost.java
@@ -0,0 +1,18 @@
+package us.dontcareabout.sic.client.util;
+
+import us.dontcareabout.sic.client.vo.Metadata;
+
+public class ImageHost {
+	//XXX 沒時間了，先寫死
+	public static final String HOST = "https://montypan.github.io/SIC";
+	public static final String TOC = "/toc";
+	public static final String PAGE = "/page";
+
+	public static String toc(int serial) {
+		return HOST + TOC + "/" + serial + ".jpg";
+	}
+
+	public static String page(Metadata md, int index) {
+		return HOST + PAGE + "/" + md.getId() + "/" + index + ".jpg";
+	}
+}
